ecd0e75a5218164ff1838ae2db63ca13a7b40178
Sebastian Ortega <sebastian.ortega.fernandezz@gmail.com>
2023-07-04 10:36:18 -0400

    cards

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -1,10 +1,13 @@
+import { HeroList } from "../components/HeroList"
+
 export const DcPage = () => {
   return (
     <>
       <div className="container">
 
         <h1>DC page</h1> 
-
+        <hr />
+       <HeroList publisher='DC Comics'/> 
       </div>
     </>
   )
--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,9 +1,13 @@
+import { HeroList } from "../components/HeroList"
 
 export const MarvelPage = () => {
   return (
    <>
       <div className="container">
         <h1>Marvel Page !</h1>
+        <hr />
+
+        <HeroList publisher='Marvel Comics'/>
       </div>
    </> 
   )
