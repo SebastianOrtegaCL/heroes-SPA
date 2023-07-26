a3ecc9ec0e2859bdc8c2a72bdacdb306a9bed33f
sebastianCl <sebastian.ortega.fernandezz@gmail.com>
2023-07-26 18:37:19 -0400

    refresh

--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -4,9 +4,8 @@ export const MarvelPage = () => {
   return (
    <>
       <div className="container">
-        <h1 className="animate__animated animate__fadeIn">Marvel Page !</h1>
+        <h1 className="animate__animated animate__fadeIn">Marvel Page</h1>
         <hr />
-
         <HeroList publisher='Marvel Comics'/>
       </div>
    </> 
