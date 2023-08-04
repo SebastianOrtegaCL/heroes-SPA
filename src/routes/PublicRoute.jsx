b6f8b7637a4828d45b8e7927f16916605f642d0c
Sebastian Ortega <sebastian.ortega.fernandezz@gmail.com>
2023-08-04 10:45:21 -0400

    testing PublicRoute.test.jsx

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -5,6 +5,7 @@ import { Navigate } from "react-router-dom"
 
 export const PublicRoute = ({children}) => {
     const { logged } = useContext(AuthContext)
+    console.log('hola mundo desde public route')
     return (!logged)
         ? children
         : <Navigate to="/marvel"/>
--- test/PublicRoute.test.jsx
+++ test/PublicRoute.test.jsx
@@ -1,38 +1,46 @@
 import {render, screen} from '@testing-library/react';
 import {PublicRoute} from "../src/routes/PublicRoute.jsx";
-import {AuthContext, AuthProvider} from "../src/auth/index.js";
-import {createMemoryRouter, Route, Routes, routes} from "react-router-dom";
+import {AuthContext, AuthProvider, LoginPage} from "../src/auth/index.js";
+import {createMemoryRouter, MemoryRouter, Route, Routes, routes} from "react-router-dom";
 
 describe('Testing in PublicRoute.jsx', function () {
-    // test('Si no esta autenticado debe de mostrar el children', () => {
-    //     const contextValue = {
-    //         logged: false,
-    //     }
-    //     render(
-    //         <AuthProvider value = { contextValue }>
-    //             <PublicRoute>
-    //                 <h1> esto es una ruta publica </h1>
-    //             </PublicRoute>
-    //         </AuthProvider>
-    //     );
-    //     // screen.debug();
-    //     expect( screen.getByText('esto es una ruta publica')).toBeTruthy();
-    // });
+    test('Si no esta autenticado debe de mostrar el children', () => {
+        const contextValue = {
+            logged: false,
+        }
+        render(
+            <AuthProvider value = { contextValue }>
+                <PublicRoute>
+                    <h1> esto es una ruta publica </h1>
+                </PublicRoute>
+            </AuthProvider>
+        );
+        // screen.debug();
+        expect( screen.getByText('esto es una ruta publica')).toBeTruthy();
+    });
 
     test('debe de navegar si esta autenticado', () => {
         const contextValue = {
-            logged: false,
-            user: {
-                name: 'Sebastian',
-                id: 'ABC123'
-            }
+            logged: true,
+            name: 'Sebastian',
         }
 
         render(
-            <AuthProvider value={contextValue} >
-
-            </AuthProvider>
+            <AuthContext.Provider value={ contextValue } >
+                <MemoryRouter initialEntries={["/login"]}>
+                    <Routes>
+                        <Route path='login' element={
+                            <PublicRoute>
+                                <h1> Ruta Publica </h1>
+                            </PublicRoute>
+                        }/>
+                        <Route path="marvel" element={<h1>Pagina Marvel</h1>}/>
+                    </Routes>
+                </MemoryRouter>
+            </AuthContext.Provider>
         )
+
+        expect( screen.getByText('Pagina Marvel')).toBeTruthy();
 
         screen.debug();
     });
